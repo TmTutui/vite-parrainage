2d7661b28d92b5e24475b4270d9d019db5ce9487
Tulio <tulio.tutui@gmail.com>
2022-10-07 19:46:35 +0200

    Fixe images/bg, add heart

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+// export { ReactComponent as HeartIcon } from '../public/heart.svg';
 
 function App() {
   const [ page, setPage ] = useState('landing')
@@ -7,9 +8,22 @@ function App() {
   const [ flipped, setFlipped ] = useState('')
 
   const [ people, setPeople ] = useState({
-    'Tulio':   { hidden_img: '/assets/test1.jpeg', normal_img: '/assets/test2.jpeg', already_selected: false},
+    'Tulio':   { hidden_img: '/assets/EliasHide.png', normal_img: '/assets/EliasShow.png', already_selected: false},
     'Machado': { hidden_img: '/assets/test1.jpeg', normal_img: '/assets/test2.jpeg', already_selected: false},
     'Enzo':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo2':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo3':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo4':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo5':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo6':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo7':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo8':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo9':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo0':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo465':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo645':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo4365':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
+    'Enzo2346':    { hidden_img: '/assets/test2.jpeg', normal_img: '/assets/test1.jpeg', already_selected: false},
   })
 
   const clickCard = (name) => {
@@ -24,23 +38,27 @@ function App() {
 
 
   if(page == 'landing') return (
-    <div style={{width: '100%', height: '100%'}} onClick={() => setPage('selection')}>
-
+    <div className='landing' onClick={() => setPage('selection')}>
+      <img src='/bg-landing.jpeg'/>
     </div>
   )
 
   else return (<section className='container'>
-    {Object.keys(people).map(name => (
+    {Object.keys(people).map((name, index) => (
       <div className={`cards ${flipped == name && 'big flipped'}`} 
-        onClick={() => clickCard(name)} key={name}
+        onClick={() => clickCard(name)} key={name} style={{left: `${index*180}px`}}
       > 
         <div className={`card ${people[name].already_selected && 'selected'}`} key={name}>
           <div className={`face front ${flipped == name && 'off'}`}> 
-            {people[name].already_selected ? <img src={people[name].hidden_img}/> : <img src={people[name].normal_img}/>}
+            {people[name].already_selected ? <img src={people[name].normal_img}/> : <img src={people[name].hidden_img}/>}
           </div> 
           <div className={`face ${flipped !== name ? 'off' : 'back'}`}> 
             <img src={people[name].normal_img}/>
-          </div> 
+          </div>
+          <div className={`heartContainer ${flipped == name && 'off'}`}>
+            <img src={'heart.svg'} className='heart'/>
+            <span>{index+1}</span>
+          </div>
         </div> 
       </div> 
     ))}
